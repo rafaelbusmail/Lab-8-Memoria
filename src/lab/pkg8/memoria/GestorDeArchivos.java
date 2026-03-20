466747a33431d09b0a0f1921a1ed86dd35664556
Diego Bejarano <diegoalejandrop78@gmail.com>
2026-03-20 10:10:15 -0600

    Metodos copiar, pegar, renombrar y crear, hechos

--- src/lab/pkg8/memoria/GestorDeArchivos.java
+++ src/lab/pkg8/memoria/GestorDeArchivos.java
@@ -26,11 +26,130 @@ public class GestorDeArchivos {
          if(nombre == null || !nombre.trim().isEmpty()){
             return new ResultadoOperacion(false, "El nombre no puede estar vacio",0,0);
         }
-          if(){
-            return new ResultadoOperacion(false, "Seleccione una carpeta en el arbol primero",0,0);
+          if(!nombreValido(nombre)){
+            return new ResultadoOperacion(false, "Nombre invalido, Evidte: \\ / : * ? \" < > | ",0,0);
         }
+          File nueva = new File(destino, nombre.trim());
+          if(nueva.exists()){
+              return new ResultadoOperacion(false, "Ya existe una carpeta llamada \""+nombre.trim()+"\".",0,0);
+          }
+          if(nueva.mkdir()){
+              return new ResultadoOperacion(false, "Carpeta \""+ nombre.trim()+"\" creada exitosamente,",1,0);
+          }else{
+             return new ResultadoOperacion(false, "No se pudo crear la carpeta.",0,1); 
+          }
     }
+    
+    public ResultadoOperacion renombrar(File archivo, String nuevoNombre){
+        if(archivo == null || !archivo.exists()){
+            return new ResultadoOperacion(false, "El elemento seleccionado no existe.",0,0);
+        }
+         if(nuevoNombre == null || nuevoNombre.trim().isEmpty()){
+            return new ResultadoOperacion(false, "El nuevo nombre no puede estar vacio",0,0);
+        }
+          if(!nombreValido(nuevoNombre)){
+            return new ResultadoOperacion(false, "Nombre invalido, Evidte: \\ / : * ? \" < > | ",0,0);
+        }
+          File destino = new File(archivo.getParent(), nuevoNombre.trim());
+          if(destino.exists()){
+              return new ResultadoOperacion(false, "Ya existe un elemento llamado \""+nuevoNombre.trim()+"\".",0,0);
+          }
+          if(destino.renameTo(destino)){
+              return new ResultadoOperacion(true, "\""+ archivo.getName()+"\" renombrado a \""+nuevoNombre.trim()+"\" exitosamente",1,0);
+          }else{
+             return new ResultadoOperacion(false, "No se pudo renombrar.",0,1); 
+          }
+    }
+    
+    public ResultadoOperacion copiar(File[] archivos){
+        if(archivos == null || archivos.length == 0){
+            return new ResultadoOperacion(false, "Seleccione al menos un elemento para copiar.",0,0); 
+        }
+        int validos = 0;
+        for (int i = 0; i < archivos.length; i++) {
+            if(archivos[i] != null && archivos[i].exists()){
+                validos++;
+            }
+        }
+        if(validos == 0){
+             return new ResultadoOperacion(false, "Ninguno de los elementos seleccionados existe.",0,0); 
+       
+        }
+        this.portapapeles = archivos;
+        String mensaje;
+        if(validos == 1){
+            mensaje = "\"" + archivos[0].getName() + "\" copiaod al portapapeles";
+        }else{
+            mensaje = validos + "Elementos copiados al portapapeles";
+        }
+        return new ResultadoOperacion(true,mensaje,validos,0);
+    }
+    
+    public ResultadoOperacion copiar(File destino){
+        if(portapapeles == null || portapapeles.length == 0){
+             return new ResultadoOperacion(false, "El portapapeles esta vacio. Copie algo primero.",0,0); 
+        }
+        if(destino == null || !destino.isDirectory()){
+             return new ResultadoOperacion(false, "Sellecione una carpeta destino en el arbol",0,0); 
+        }
+        int pegados = 0;
+        int errores = 0;
+        for (int i = 0; i < portapapeles.length; i++) {
+            File item =  portapapeles[i];
+            if(item == null || !item.exists()){
+                errores++;
+                continue;
+            }
+            File archivoDestino = new File(destino, item.getName());
+            if(archivoDestino.exists()){
+                archivoDestino = generarNombreUnico(destino, item.getName());
+            }
+            boolean exito;
+            if(item.isDirectory()){
+                exito = copiarCarpetaRecursivo(item, archivoDestino);
+            }else{
+                exito = copiarArchivo(item, archivoDestino);
+            }
+            if(exito){
+                pegados++;
+            }else{
+                errores ++;
+            }
+        }
+        String hayerrores = "";
+        if(errores > 0){
+            hayerrores = "Erroes" + errores;
+        }else{
+            hayerrores = "Sin errores";
+        }
+    
+        portapapeles = new File[0];
+        String mensaje = "Pegado completo.\n"
+                + "Elementos pegados: "+ pegados + "\n"
+                + hayerrores;
+        return new ResultadoOperacion(pegados > 0, mensaje, pegados, errores);
+    }
+    
     private boolean nombreValido(String nombre){
+        if(nombre == null || nombre.trim().isEmpty()){
+            return false;
+        }
+        String[] prohibidos = {"\\","/",":","*","?","\"","<",">","|"};
+        for(int i = 0; i < prohibidos.length;i++){
+            if(nombre.contains(prohibidos[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+    private File generarNombreUnico(File carpeta, String nombreOriginal){
         
     }
+    private boolean copiarCarpetaRecursivo(File origen, File destino){
+        
+    }
+    private boolean copiarArchivo(File origen, File destino){
+        
+    }
+    
 }
--- src/lab/pkg8/memoria/ResultadoOperacion.java
+++ src/lab/pkg8/memoria/ResultadoOperacion.java
@@ -9,5 +9,54 @@ package lab.pkg8.memoria;
  * @author diego
  */
 class ResultadoOperacion {
+    private boolean exito;
+    private String mensaje;
+    private int elementosProcesados;
+    private int errores;
+
+    public ResultadoOperacion(boolean exito, String mensaje, int elementosProcesados, int errores) {
+        this.exito = exito;
+        this.mensaje = mensaje;
+        this.elementosProcesados = elementosProcesados;
+        this.errores = errores;
+    }
+
+    public boolean isExito() {
+        return exito;
+    }
+
+    public void setExito(boolean exito) {
+        this.exito = exito;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public int getElementosProcesados() {
+        return elementosProcesados;
+    }
+
+    public void setElementosProcesados(int elementosProcesados) {
+        this.elementosProcesados = elementosProcesados;
+    }
+
+    public int getErrores() {
+        return errores;
+    }
+
+    public void setErrores(int errores) {
+        this.errores = errores;
+    }
+
+    @Override
+    public String toString() {
+        return  mensaje;
+    }
+    
     
 }
